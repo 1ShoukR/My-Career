c8e811bd3ce43a6fc527dc9644e08cf460d2e6a8
1ShoukR <rahminshoukoohi@gmail.com>
2022-06-08 09:59:01 -0400

    finsihed dice

--- week1/day2/RealHomeworkAssignedByJoe/DiceRoller.py
+++ week1/day2/RealHomeworkAssignedByJoe/DiceRoller.py
@@ -7,26 +7,24 @@ import random
 sides = int(input("How many sides should it have? (2-20): "))
 
 side2 = random.randrange(1, 2)
-
-side3 = random.randrange(1, 6)
-
-side4 = random.randrange(1, 12)
-side5 = random.randrange(1, )
-side6 = random.randrange(1, )
-side7 = random.randrange(1, )
-side8 = random.randrange(1, )
-side9 = random.randrange(1, )
-side10 = random.randrange(1, )
-side11 = random.randrange(1, )
-side12 = random.randrange(1, )
-side13 = random.randrange(1, )
-side14 = random.randrange(1, )
-side15 = random.randrange(1, )
-side16 = random.randrange(1, )
-side17 = random.randrange(1, )
-side18 = random.randrange(1, )
-side19 = random.randrange(1, )
-side20 = random.randrange(1, )
+side3 = random.randrange(1, 3)
+side4 = random.randrange(1, 4)
+side5 = random.randrange(1, 5)
+side6 = random.randrange(1, 6)
+side7 = random.randrange(1, 7)
+side8 = random.randrange(1, 8)
+side9 = random.randrange(1, 9)
+side10 = random.randrange(1, 10)
+side11 = random.randrange(1, 11)
+side12 = random.randrange(1, 12)
+side13 = random.randrange(1, 13)
+side14 = random.randrange(1, 14)
+side15 = random.randrange(1, 15)
+side16 = random.randrange(1, 16)
+side17 = random.randrange(1, 17)
+side18 = random.randrange(1, 18)
+side19 = random.randrange(1, 19)
+side20 = random.randrange(1, 20)
 
 
 
@@ -46,13 +44,46 @@ elif sides == 5:
 elif sides == 6:
     print("rolling dice..")
     print(f"you got", side6)
-elif sides == 4:
+elif sides == 7:
     print("rolling dice..")
     print(f"you got", side7)
-elif sides == 4:
+elif sides == 8:
     print("rolling dice..")
     print(f"you got", side8)
+elif sides == 9:
+    print("rolling dice..")
+    print(f"you got", side9)
+elif sides == 10:
+    print("rolling dice..")
+    print(f"you got", side10)
+elif sides == 11:
+    print("rolling dice..")
+    print(f"you got", side11)
+elif sides == 12:
+    print("rolling dice..")
+    print(f"you got", side12)
+elif sides == 13:
+    print("rolling dice..")
+    print(f"you got", side13)
+elif sides == 14:
+    print("rolling dice..")
+    print(f"you got", side14)
+elif sides == 15:
+    print("rolling dice..")
+    print(f"you got", side15)
+elif sides == 16:
+    print("rolling dice..")
+    print(f"you got", side16)
+elif sides == 17:
+    print("rolling dice..")
+    print(f"you got", side17)
+elif sides == 18:
+    print("rolling dice..")
+    print(f"you got", side18)
+elif sides == 19:
+    print("rolling dice..")
+    print(f"you got", side19)
+elif sides == 20:
+    print("rolling dice..")
+    print(f"you got", side20)
     
-
-
-

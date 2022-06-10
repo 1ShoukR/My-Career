6d37a6822d4f80e9dc3fbf093a65409aaf3811b5
1ShoukR <rahminshoukoohi@gmail.com>
2022-06-10 01:20:57 -0400

    Update myFirstGame.py

--- week1/day5/myFirstGame.py
+++ week1/day5/myFirstGame.py
@@ -1,5 +1,35 @@
+from csv import list_dialects
+from logging import logProcesses
 from tkinter.messagebox import YES
 import sys
+from turtle import left, right
+from time import sleep
+
+
+
+
+# to do list: 
+# use while loops 
+# introduce list 
+# use a dictionary
+# loop and nested loop 
+# make break proof and make user input what i want
+
+
+
+
+
+
+def HallwayOne():
+    print("You have two choices: Left or Right. Which way do you go? ")
+    choice = input("---> ")
+    if choice == 'right':
+        print("Nice! You made it through")
+    return("You've been captured! You will be executed immediately. GGs ")
+
+
+
+
 
 print("""Hello! My name is Sherry! 
 I'll be your eyes and ears while you're inside the Empire's battlestation 
@@ -7,7 +37,9 @@ I'll be your eyes and ears while you're inside the Empire's battlestation
 ***************************************
 
 """)
+sleep(3)
 print("Looks like you guys are in cell block three.")
+sleep(3)
 
 
 def panel():
@@ -53,9 +85,11 @@ myAnswer = panelButtons()
 
 if int(myAnswer) == 1:
      print("You turned the incinerator on. GGs bro nt..")
+     exit(0)
 elif myAnswer == 2:
     print("""That button alerted high command.
 They're sending troops to your cell block.. GGs bro... """)
+    exit(0)
 elif myAnswer == 3:
     print("""That opened the door and shut the security cameras off!
 Now I cannot draw the map of the layout on my secret spy tablet.
@@ -64,21 +98,21 @@ schematics of the base onto the tablet!
 
 ***************************************
 """)
+    HallwayOne()
+        
 elif myAnswer == 4:
     print("""Oh god.. That button just alerted the bridge..
 They are sending a task force squad to your cell..
 I've been locked out of the system! I cannot help, I'm sorry..
 They will execute you on site.. GGs bros go next""" )
+    exit(0)
 elif myAnswer == 5:
     print("""This button literally did nothing.
 Bro just click button 3 man don't make this difficult for yourself """)
+    exit(0)
 
 
-def directionHallwaysOne():
-        while True:
-            myDirection = input("""Which way do you wanna go? 
- Left or Right? """)
-directionHallwaysOne()
+
 
 
 

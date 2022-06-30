0c7bda64b210bf2552cd77502eb6a36918acb568
1ShoukR <rahminshoukoohi@gmail.com>
2022-06-30 16:26:44 -0400

    blackJack js game
    more updates

--- BlackJack js game/scripts/main.js
+++ BlackJack js game/scripts/main.js
@@ -1,15 +1,19 @@
 const dealerHand = document.getElementById("dealer-hand")
 const playerHand = document.getElementById("player-hand")
 
+const dealerPoints = document.getElementById("dealer-points")
+const playerPoints = document.getElementById("player-points")
+
+
 const dealBtn = document.getElementById("deal-button")
 const hitBtn = document.getElementById("hit-button")
 const standBtn = document.getElementById("stand-button")
 
-const deck = []
+let deck = []
 const suits = ["hearts", "spades", "clubs", "diamonds"]
 const ranks = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-const playerCards = []
-const dealerCards = []
+let playerCards = []
+let dealerCards = []
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -49,7 +53,7 @@ const addFirstDealerCard = () =>{
   dealerCard1Suit = dealerCardOneSuit
   console.log(deck)
   dealerCards.push(dealerCard1sum + dealerCard1Suit);
-  dealerHand.append("dealer first card is the ", dealerCard1sum , " of ", dealerCard1Suit, ". ")
+  dealerHand.append("card: ", dealerCard1sum , " of ", dealerCard1Suit, ". ")
 }
 const addSecondDealerCard = () =>{
   const dealerCardTwo = playerHandDraw()
@@ -60,7 +64,7 @@ const addSecondDealerCard = () =>{
   dealerCard2Suit = dealerCardTwoSuit
   console.log(deck)
   dealerCards.push(dealerCard2Sum + dealerCard2Suit)
-  dealerHand.append("Dealer second card is the ", dealerCard2Sum, " of ", dealerCard2Suit, ". ")
+  dealerHand.append("card: ", dealerCard2Sum, " of ", dealerCard2Suit, ". ")
 }
 
 const addFirstPlayerCard = () =>{
@@ -72,7 +76,7 @@ const addFirstPlayerCard = () =>{
   playerCard1Suit = playerCardOneSuit
   console.log(deck)
   playerCards.push(playerCard1Sum + playerCard1Suit)
-  playerHand.append("your first card is the ", playerCard1Sum, " of ", playerCard1Suit, ". ")
+  playerHand.append("card: ", playerCard1Sum, " of ", playerCard1Suit, ". ")
 }
 const addSecondPlayerCard = () => {
   const playerCardTwo = playerHandDraw();
@@ -83,8 +87,40 @@ const addSecondPlayerCard = () => {
   playerCard2Suit = playerCardTwoSuit
   console.log(deck);
   playerCards.push(playerCard2Sum + playerCard2Suit)
-  playerHand.append('Your second card is the ', playerCard2Sum, " of ", playerCard2Suit, ". ");
+  playerHand.append('card: ', playerCard2Sum, " of ", playerCard2Suit, ". ");
 }
+
+const renderCard = (card, targetElement) =>{
+  const img = document.createElement("img")
+  img.src = "./images/" + card.rank + "_of_" + card.suit + ".png"
+  targetElement.append(img)
+}
+
+
+const sumOfcards = (targetCards) =>{
+  let sum = 0
+  for (let card of targetCards){
+    sum += card.value.primary
+  }
+  return sum
+}
+
+const renderDealerSum = () =>{
+  const dealerCardSum = sumOfcards(dealerCards)
+  dealerPoints.innerHTML = null
+  dealerPoints.append(dealerCardSum)
+}
+
+const renderPlayerSum = () =>{
+  const playerCardSum = sumOfcards(playerCards)
+}
+
+
+
+
+
+
+
 
 
 const gameStart = () => {
@@ -98,9 +134,37 @@ const gameStart = () => {
   console.log("player", playerCards)
 }
 
-gameStart()
+const inGame = () =>{
+  deck = []
+  dealerCards = []
+  playerCards = []
+}
 
-dealBtn.onclick = gameStart
+
+
+
+const restartGame = () =>{
+  setTimeout(inGame, 2000)
+  setTimeout(createCard)
+}
+
+
+
+dealBtn.onclick = () =>{
+  dealBtn.setAttribute("disabled", "disabled")
+  gameStart();
+  console.log(dealerCards)
+  console.log(playerCards)
+}
+
+hitBtn.onclick = () =>{
+  addFirstPlayerCard()
+  console.log(playerCards)
+}
+
+standBtn.onclick = () =>{
+
+}
 
 
 

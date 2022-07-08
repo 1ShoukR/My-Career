6e58ccfe8a47063348b0c0db22000c0277dce849
1ShoukR <rahminshoukoohi@gmail.com>
2022-07-08 02:02:14 -0400

    Finished creating HTML and base function of the javascript.

--- week6/Front-End-Project/js files/rankings.js
+++ week6/Front-End-Project/js files/rankings.js
@@ -1,3 +1,13 @@
-const rankingsURL = `https://vlrggapi.herokuapp.com/rankings/na`;
-// Hello
-// how are you doing 
+const container = document.getElementById('container')
+const searchButton = document.getElementById('searchButton');
+
+
+const getTeam = async ()=>{
+    const inputField = document.getElementById('inputField').value
+    const rankingsURL = `https://vlrggapi.herokuapp.com/rankings/${inputField}`;
+    const stats = await fetch(rankingsURL)
+    const statsJson = await stats.json()
+    console.log(statsJson)
+    console.log(inputField);
+}
+searchButton.onclick = () =>  getTeam()

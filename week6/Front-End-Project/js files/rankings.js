c90404be82798507b98134793cd30b80627b494f
1ShoukR <rahminshoukoohi@gmail.com>
2022-07-08 15:11:20 -0400

    front end project update

--- week6/Front-End-Project/js files/rankings.js
+++ week6/Front-End-Project/js files/rankings.js
@@ -1,5 +1,6 @@
 const container = document.getElementById('container')
 const searchButton = document.getElementById('searchButton');
+const teamPages = document.getElementById('teamPages');
 
 
 const getTeam = async ()=>{
@@ -8,6 +9,21 @@ const getTeam = async ()=>{
     const stats = await fetch(rankingsURL)
     const statsJson = await stats.json()
     console.log(statsJson)
-    console.log(inputField);
+    console.log(inputField)
+    for (index in statsJson.data){
+        var team = statsJson.data[index]
+        console.log(team)
+        const teamPage = document.createElement('p')
+        const teamLogo = document.createElement('img')
+        teamLogo.classList = 'teamLogo'
+        teamLogo.src = statsJson.data.logo
+        teamPage.classList = 'teamPage'
+        teamPages.append(teamPage, teamLogo)
+        for (var item of Object.keys(statsJson.data[index])){
+            let contentSpace = ' '
+            teamPage.append(statsJson.data[index][item] + contentSpace, teamLogo)
+        }
+        // teamPage.append(' ' + statsJson.data[index].team)
+    }
 }
 searchButton.onclick = () =>  getTeam()
---
+++ week6/algo friday/classPhotos.js
@@ -0,0 +1,13 @@
+const redShirtHeights = [5,8,1,3,4]
+const blueShirtHeights = [6,9,2,4,5]
+console.log(newRed)
+console.log(newBlue)
+
+
+const classPhotos = () =>{
+    newRed = redShirtHeights.sort()
+    newBlue = blueShirtHeights.sort()
+    for (let index = 0; index < newRed.length; index++) {
+        newRed[index] < newBlue[index]
+    }
+}

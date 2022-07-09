1b32f20812ebbed7a3b431289d28f31d2db9d31a
1ShoukR <rahminshoukoohi@gmail.com>
2022-07-08 21:06:12 -0400

    front end project update
    got playerStats working

--- week6/Front-End-Project/js files/playerStats.js
+++ week6/Front-End-Project/js files/playerStats.js
@@ -1,3 +1,30 @@
 // api info: 
 // need to put ${region} and ${timespan} in url (make this attached to input field. Look at weather app for reference)
-const statsURL = `https://vlrggapi.herokuapp.com/stats/na`;
+// statsJson.data.segments
+
+const btn = document.getElementById('btn')
+const container = document.getElementById('container')
+const statsPage = document.getElementById('statsPage')
+
+console.log(inputField.value)
+
+
+const getPlayerStats = async () =>{
+    const inputField = document.getElementById('inputField').value;
+    const statsURL = `https://vlrggapi.herokuapp.com/stats/${inputField}`;
+    const stats = await fetch(statsURL)
+    const statsJson = await stats.json()
+    console.log(statsJson)
+    console.log(inputField)
+    for (index in statsJson.data.segments){
+        var players = statsJson.data.segments[index];
+        console.log(players)
+        const playerStats = document.createElement('p')
+        playerStats.classList = 'playerStats'
+        statsPage.append(playerStats)
+        for (var item of Object.keys(statsJson.data.segments[index])){
+            playerStats.append(statsJson.data.segments[index][item] + ' | ')
+        }
+    }
+}
+btn.onclick = getPlayerStats

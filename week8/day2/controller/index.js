73f3ca8120535aae5ce9a416e48bd44052d44604
1ShoukR <rahminshoukoohi@gmail.com>
2022-07-26 22:13:20 -0400

    added ownerRoutes to controller

--- week8/day2/controller/index.js
+++ week8/day2/controller/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const es6Renderer = require('express-es6-template-engine');
 const PORT = 3005;
 const petRoutes = require('./routes/pets');
+const ownerRoutes = require('./routes/owners')
 // middleware
 app.use(express.static('public'));
 app.engine('html', es6Renderer);
@@ -12,6 +13,7 @@ app.set('view engine', 'html');
 app.use(express.json())
 app.use(cors())
 app.use("/pets", petRoutes)
+app.use('/owners', ownerRoutes)
 
 
 

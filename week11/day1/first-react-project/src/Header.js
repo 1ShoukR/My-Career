f5d8c0ffc8fda1d3f518e55e2bde0f55b6a02e0f
1ShoukR <rahminshoukoohi@gmail.com>
2022-08-15 23:59:51 -0400

    Update Header.js

--- week11/day1/first-react-project/src/Header.js
+++ week11/day1/first-react-project/src/Header.js
@@ -2,5 +2,7 @@ import React from 'react';
 import "./App.css"
 
 export default function Header() {
-  return <div className= "header">Header</div>;
+  return <div className= "header">
+    <h1>Welcome to my To Do List!</h1>
+  </div>;
 }

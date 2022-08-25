c8a48516f6c7430a09031e212e5027453a403a09
1ShoukR <rahminshoukoohi@gmail.com>
2022-08-25 00:55:53 -0400

    Update Navbar.js

--- react-frontend-project/the-smugglers-den/src/components/Navbar.js
+++ react-frontend-project/the-smugglers-den/src/components/Navbar.js
@@ -5,6 +5,9 @@ export const Navbar = () => {
 return (
     <div>
         <nav className="navbar">
+        <Link to="/people">People</Link>
+        <Link to="/starships">Starships</Link>
+        <Link to="/movies">Movies</Link>
         <Link to="/">Home</Link>
         <Link to="/tos">Terms of services</Link>
         </nav>

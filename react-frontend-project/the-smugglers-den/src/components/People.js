3bef2301b9e8d5a50c9a70340c3bdf85b9502e9f
1ShoukR <rahminshoukoohi@gmail.com>
2022-08-25 00:55:55 -0400

    Update People.js

--- react-frontend-project/the-smugglers-den/src/components/People.js
+++ react-frontend-project/the-smugglers-den/src/components/People.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
 const People = () => {
-  return (
+    const fetchPeople = async () => {
+        const url = `https://swapi.dev/api/people/`;
+        const res = await fetch(url);
+        const peopleData = await res.json();
+        console.log('people Data', peopleData);
+    };
+    fetchPeople();
+return (
     <div>
         <h1>
             The People of Star Wars
         </h1>
     </div>
-  )
+)
 }
 
 export default People

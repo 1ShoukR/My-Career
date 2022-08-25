0ee2bdc8e5f9115343ad78e10b2ace1b8a5cc59d
1ShoukR <rahminshoukoohi@gmail.com>
2022-08-25 00:55:49 -0400

    Update App.js

--- react-frontend-project/the-smugglers-den/src/App.js
+++ react-frontend-project/the-smugglers-den/src/App.js
@@ -7,29 +7,6 @@ import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
   const dispatch = useDispatch();
-  const fetchPeople = async () =>{
-    const url = `https://swapi.dev/api/people/`;
-    const res = await fetch(url)
-    const peopleData = await res.json()
-    console.log("people Data", peopleData)
-  }
-  fetchPeople()
-
-  const fetchMovies = async () =>{
-    const url = `https://swapi.dev/api/films/`;
-    const res = await fetch(url)
-    const movieData = await res.json()
-    console.log("movie data",movieData)
-  }
-  fetchMovies()
-
-  const fetchStarships = async () => {
-    const url = `https://swapi.dev/api/starships/9/`;
-    const res = await fetch(url)
-    const starshipData = await res.json()
-    console.log("starship data",starshipData)
-  }
-  fetchStarships()
   return (
     <div className="App">
       <h1>The Smuggler's Den</h1>

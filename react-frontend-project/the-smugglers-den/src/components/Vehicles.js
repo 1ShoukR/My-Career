66a57dcc98e414c35b645c8097b392aee97445ca
1ShoukR <rahminshoukoohi@gmail.com>
2022-08-28 22:50:43 -0400

    Update Vehicles.js

--- react-frontend-project/the-smugglers-den/src/components/Vehicles.js
+++ react-frontend-project/the-smugglers-den/src/components/Vehicles.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'; 
 import {fetchVehicles, fetchVehiclesPage2, fetchVehiclesPage3, fetchVehiclesPage4} from '../actions/fetchVehicles';
 import { VehicleCard } from './VehicleCard';
+import Button from '@mui/material/Button';
 
 
 
@@ -9,19 +10,24 @@ const Vehicles = () => {
 const dispatch = useDispatch()
 const vehicle = useSelector((state) => state.vehicles)
   return (
-    <div>
+    <div className='vehiclesContainer'>
+      <div className='vehiclesTitle'>
         <h1>Vehicles</h1>
-        <button onClick={() => {fetchVehicles(dispatch)}}>Get Vehicles</button>
-        <h1>{vehicle?.results?.map((item) =>{
+      </div>
+        <Button variant="outlined" size="large" onClick={() => {fetchVehicles(dispatch)}}>Get Vehicles</Button>
+        <h1 className='h1VehicleCard'>{vehicle?.results?.map((item) =>{
           return (
             <>
               <VehicleCard item={item}/>
             </>
           )
         })}</h1>
-        <button onClick={() =>{fetchVehiclesPage2(dispatch)}}>Page 2</button>
-        <button onClick={() =>{fetchVehiclesPage3(dispatch)}}>Page 3</button>
-        <button onClick={() =>{fetchVehiclesPage4(dispatch)}}>Page 4</button>
+        <div className='nextPageButtons'>
+        <Button variant="outlined" size="large" onClick={() =>{fetchVehiclesPage2(dispatch)}}>Page 2</Button>
+        <Button variant="outlined" size="large" onClick={() =>{fetchVehiclesPage3(dispatch)}}>Page 3</Button>
+        <Button variant="outlined" size="large" onClick={() =>{fetchVehiclesPage4(dispatch)}}>Page 4</Button>
+        </div>
+        
     </div>
   )
 }

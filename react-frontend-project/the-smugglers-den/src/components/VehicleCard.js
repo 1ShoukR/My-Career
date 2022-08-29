7a16069db8df310fadcc93510402a0ee3b0b34c3
1ShoukR <rahminshoukoohi@gmail.com>
2022-08-28 22:50:40 -0400

    Update VehicleCard.js

--- react-frontend-project/the-smugglers-den/src/components/VehicleCard.js
+++ react-frontend-project/the-smugglers-den/src/components/VehicleCard.js
@@ -1,19 +1,46 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import setFavorites from '../actions/setFavorites';
+import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
 
 export const VehicleCard = (props) => {
 const dispatch = useDispatch()
   return (
     <div>
-        <h1>Vehicle: {props.item.name}</h1>
-        <p>Model: {props.item.model}</p>
-        <p>Passenger Capacity: {props.item.passengers}</p>
-        <p>Crew: {props.item.crew}</p>
-        <p>Manufacteror: {props.item.model}</p>
-        <p>Max Speed: {props.item.max_atmospheric_speed}</p>
-        <p>Cost: {props.item.cost_in_credits} Credits</p>
-        <button onClick={()=>{setFavorites(dispatch, props);}}>Add to Favorites</button>
+      <Card className="vehicleCardContainer">
+        <div className="vehicleTitle">
+          <h1>Vehicle: {props.item.name}</h1>
+        </div>
+        <div className="modelName">
+          <p>Model: {props.item.model}</p>
+        </div>
+        <div className="passengerCapacity">
+          <p>Passenger Capacity: {props.item.passengers}</p>
+        </div>
+        <div className='crew'>
+          <p>Crew: {props.item.crew}</p>
+        </div>
+        <div className='manufacteror'>
+          <p>Manufacteror: {props.item.model}</p>
+        </div>
+        <div className='maxSpeed'>
+          <p>Max Speed:{props.item.max_atmosphering_speed} KM/H</p>
+        </div>
+        <div className='cost'>
+          <p>Cost: {props.item.cost_in_credits} Credits</p>
+        </div>
+      </Card>
+      <Button
+      className='favoritesButton'
+        variant="outlined"
+        size="large"
+        onClick={() => {
+          setFavorites(dispatch, props);
+        }}
+      >
+        Add to Favorites
+      </Button>
     </div>
-  )
+  );
 }

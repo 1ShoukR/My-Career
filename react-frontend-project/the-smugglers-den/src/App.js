5f30111684c2a11bf3a7c8de1cb77ea4d7c4ab82
1ShoukR <rahminshoukoohi@gmail.com>
2022-08-28 22:50:36 -0400

    Update App.js

--- react-frontend-project/the-smugglers-den/src/App.js
+++ react-frontend-project/the-smugglers-den/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux'; 
+import { useDispatch } from 'react-redux'; 
 import Movies from './components/Movies';
 
 
@@ -12,33 +12,33 @@ function App() {
     <div className="App">
       <h1>The Smuggler's Den</h1>
       <h5>All your Star Wars needs, in one place!</h5>
-      <img
+      {/* <img
         src="https://seeklogo.com/images/S/Star_Wars-logo-2B2C24F703-seeklogo.com.png"
         alt="logo"
-      />
-      <div>
-        <h4>Click any of the buttons to go to your favorite page!</h4>
-        <div>
-          <Link to="/movies">
-            <button>Movies</button>
+      /> */}
+      <div className="mainPageContainer">
+        <div className="mainPageComponents">
+          <h4>Click any of the buttons to go to your favorite page!</h4>
+          <Link className="mainPageButtons" to="/movies">
+            <button className="bootons">Movies</button>
           </Link>
-          <Link to="/people">
-            <button>People</button>
+          <Link className="mainPageButtons" to="/people">
+            <button className="bootons">People</button>
           </Link>
-          <Link to="planets">
-            <button>Planets</button>
+          <Link className="mainPageButtons" to="planets">
+            <button className="bootons">Planets</button>
           </Link>
-          <Link to="species">
-            <button>Species</button>
+          <Link className="mainPageButtons" to="species">
+            <button className="bootons">Species</button>
           </Link>
-          <Link to="/starships">
-            <button>Starships</button>
+          <Link className="mainPageButtons" to="/starships">
+            <button className="bootons">Starships</button>
           </Link>
-          <Link to="vehicles">
-            <button>Vehicles</button>
+          <Link className="mainPageButtons" to="vehicles">
+            <button className="bootons">Vehicles</button>
           </Link>
-          <Link to="favorites">
-            <button>Favorites</button>
+          <Link className="mainPageButtons" to="favorites">
+            <button className="bootons">Favorites</button>
           </Link>
           <div>
             <h2>Time to get nerdy!</h2>
